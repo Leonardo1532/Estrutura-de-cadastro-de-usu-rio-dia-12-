bf0afcb04000e11fa1f1fcb48c66ec5c2b8c0a42
Leonardo Santos Silva <1532leleo@gmail.com>
2024-01-26 09:40:53 -0300

    Fluxo criado e switch/case  base criado

---
+++ index.js
@@ -0,0 +1,47 @@
+// 1 - Crie 2 arrays: um para nomes e um para senhas
+
+
+// 2- Solicite ao seu usuário o que ele deseja fazer: cadastrar, fazer login, excluir um cadastro ou encerrar o programa.
+
+
+// 3 - Faça o fluxo de funcionamento do código, considere que o usuário pode escolher quando encerrar o programa conforme as opções.
+
+
+// 4 - Se a opção escolhida for cadastro, solicite dois valores ao usuário, um nome e uma senha e guarde cada um no seu respectivo array.
+
+// 5 - Se a opção escolhida for login solicite ao usuário um nome e uma senha, depois procure o nome recebido no array de nomes e em caso positivo compare a senha no array de senhas. Se as duas comparações forem válidas exiba uma mensagem de sucesso
+
+
+// 6 - Se a opção escolhida for excluir um cadastro solicite um nome ao usuário, então procure pelo nome no array de nomes e exclua ele e a senha correspondente do outro array, por fim organize o array para eliminar os espaços vazios.
+
+
+let nomes = []
+let senhas = []
+let index = 0
+
+
+let fluxo = true
+
+while (fluxo) {
+
+    let opcao = Number(prompt("Insira oque deseja fazer: 1 = cadastrar, 2 = fazer login, 3 = excluir um cadastro, 4 = encerrar o programa"))
+
+    switch (opcao) {
+        case 1:
+            
+            break;
+        case 2:
+            
+
+            break;
+        case 3:
+            
+
+            break;
+        case 4:
+            
+
+        default:
+            break;
+    }
+}
